6cd99f0ddd531329ec60b8e49729660eee211764
krishpranav <krisna.pranav@gmail.com>
2021-05-06 15:48:43 +0530

    added initalize function, type structers

--- main.go
+++ main.go
@@ -15,3 +15,28 @@ import (
 
 	"github.com/gizak/termui"
 )
+
+const statFilePath = "/proc/stat"
+const meminfoFilePath = "/proc/meminfo"
+const netinfoFilePath = "/proc/net/dev"
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+type ProcessList struct {
+
+}
+
+type CpuStat struct {
+	user 	float32
+	nice 	float32
+	system	float32
+	idle 	float32
+}
+
+type CpusStats struct {
+	stat map[string]CpuStat
+	proc map[string]CpuStat
+}
+
