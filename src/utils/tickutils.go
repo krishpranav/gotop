2aceb4aa2b5117bcff1b8ccbdf973f96586ad0c8
krishpranav <krisna.pranav@gmail.com>
2021-07-01 15:55:26 +0530

    [+] ticker [+]

--- src/utils/tickutils.go
+++ src/utils/tickutils.go
@@ -4,3 +4,15 @@ import (
 	"context"
 	"time"
 )
+
+func TickUntilDone(ctx context.Context, refreshRate int64, action func() error) (err error) {
+	ticker := time.NewTicker(time.Duration(refreshRate) * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		err := action()
+		if err != nil {
+			return err
+		}
+	}
+}
