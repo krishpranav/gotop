de848d7bd8a7fe57763e294e14e2ab87fc3c4879
krishpranav <krisna.pranav@gmail.com>
2021-07-01 15:32:15 +0530

    [+] bar chart struct [+]

--- src/utils/barGraph.go
+++ src/utils/barGraph.go
@@ -8,3 +8,16 @@ import (
 
 	ui "github.com/gizak/termui/v3"
 )
+
+type BarChart struct {
+	NumFormatter func(float64) string
+	Labels       []string
+	BarColors    []ui.Color
+	LabelStyles  []ui.Style
+	NumStyles    []ui.Style
+	Data         []float64
+	ui.Block
+	BarWidth int
+	BarGap   int
+	MaxVal   float64
+}
