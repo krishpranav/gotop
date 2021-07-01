4c1aa38edeb1f117bae9bdd8cc308f8da6120fa8
krishpranav <krisna.pranav@gmail.com>
2021-07-01 16:06:28 +0530

    [+] serve data [+]

---
+++ src/container/serveData.go
@@ -0,0 +1,42 @@
+package container
+
+import (
+	"context"
+
+	"github.com/docker/docker/client"
+	"github.com/pesos/grofer/src/utils"
+)
+
+func Serve(ctx context.Context, cli *client.Client, all bool, dataChannel chan ContainerMetrics, refreshRate int64) error {
+	return utils.TickUntilDone(ctx, refreshRate, func() error {
+		metrics, err := GetOverallMetrics(ctx, cli, all)
+		if err != nil {
+			return err
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case dataChannel <- metrics:
+		}
+
+		return nil
+	})
+}
+
+func ServeContainer(ctx context.Context, cli *client.Client, cid string, dataChannel chan PerContainerMetrics, refreshRate int64) error {
+	return utils.TickUntilDone(ctx, refreshRate, func() error {
+		metrics, err := GetContainerMetrics(ctx, cli, cid)
+		if err != nil {
+			return err
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case dataChannel <- metrics:
+		}
+
+		return nil
+	})
+}
