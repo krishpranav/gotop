adc111b41fd4fc57cbef3fc86f989221954e8301
krishpranav <krisna.pranav@gmail.com>
2021-07-02 08:12:02 +0530

    added round off cpu usage calculation function, get cpu rates, serve cpu rates

--- src/general/serveStats.go
+++ src/general/serveStats.go
@@ -13,3 +13,34 @@ import (
 	"github.com/shirou/gopsutil/mem"
 	"github.com/shirou/gopsutil/net"
 )
+
+func roundOff(num uint64) float64 {
+	x := float64(num) / (1024 * 1024 * 1024)
+	return math.Round(x*10) / 10
+}
+
+func GetCPURates() ([]float64, error) {
+	cpuRates, err := cpu.Percent(time.Second, true)
+	if err != nil {
+		return nil, err
+	}
+	return cpuRates, nil
+}
+
+func ServeCPURates(ctx context.Context, cpuChannel chan utils.DataStats) error {
+	cpuRates, err := cpu.Percent(time.Second, true)
+	if err != nil {
+		return err
+	}
+	data := utils.DataStats{
+		CpuStats: cpuRates,
+		FieldSet: "CPU",
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case cpuChannel <- data:
+		return nil
+	}
+}
