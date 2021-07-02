bede89485f7d4a683122e0b9a0505b8315b675ec
krishpranav <krisna.pranav@gmail.com>
2021-07-02 08:13:05 +0530

    serve cpu, memory stats functions

--- src/general/serveStats.go
+++ src/general/serveStats.go
@@ -44,3 +44,91 @@ func ServeCPURates(ctx context.Context, cpuChannel chan utils.DataStats) error {
 		return nil
 	}
 }
+
+func ServeMemRates(ctx context.Context, dataChannel chan utils.DataStats) error {
+	memory, err := mem.VirtualMemory()
+	if err != nil {
+		return err
+	}
+
+	memRates := []float64{roundOff(memory.Total), roundOff(memory.Available), roundOff(memory.Used), roundOff(memory.Free)}
+
+	data := utils.DataStats{
+		MemStats: memRates,
+		FieldSet: "MEM",
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case dataChannel <- data:
+		return nil
+	}
+}
+
+func ServeDiskRates(ctx context.Context, dataChannel chan utils.DataStats) error {
+	var partitions []disk.PartitionStat
+	var err error
+	partitions, err = disk.Partitions(false)
+	if err != nil {
+		return err
+	}
+
+	rows := [][]string{{"Mount", "Total", "Used %", "Used", "Free", "FS Type"}}
+	for _, value := range partitions {
+		usageVals, _ := disk.Usage(value.Mountpoint)
+
+		if strings.HasPrefix(value.Device, "/dev/loop") {
+			continue
+		} else if strings.HasPrefix(value.Mountpoint, "/var/lib/docker") {
+			continue
+		} else {
+
+			path := usageVals.Path
+			total := fmt.Sprintf("%.2f G", float64(usageVals.Total)/(1024*1024*1024))
+			used := fmt.Sprintf("%.2f G", float64(usageVals.Used)/(1024*1024*1024))
+			usedPercent := fmt.Sprintf("%.2f %s", usageVals.UsedPercent, "%")
+			free := fmt.Sprintf("%.2f G", float64(usageVals.Free)/(1024*1024*1024))
+			fs := usageVals.Fstype
+			row := []string{path, total, usedPercent, used, free, fs}
+			rows = append(rows, row)
+
+		}
+	}
+
+	data := utils.DataStats{
+		DiskStats: rows,
+		FieldSet:  "DISK",
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case dataChannel <- data:
+		return nil
+	}
+}
+
+func ServeNetRates(ctx context.Context, dataChannel chan utils.DataStats) error {
+	netStats, err := net.IOCounters(false)
+	if err != nil {
+		return err
+	}
+	IO := make(map[string][]float64)
+	for _, IOStat := range netStats {
+		nic := []float64{float64(IOStat.BytesSent) / (1024), float64(IOStat.BytesRecv) / (1024)}
+		IO[IOStat.Name] = nic
+	}
+
+	data := utils.DataStats{
+		NetStats: IO,
+		FieldSet: "NET",
+	}
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case dataChannel <- data:
+		return nil
+	}
+}
