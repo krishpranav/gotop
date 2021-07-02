81bf6246bb98f68f6bab14532f220dea2fff6c4b
krishpranav <krisna.pranav@gmail.com>
2021-07-02 08:24:54 +0530

    completion will add the default path for activating gotop

---
+++ cmd/completion.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var completionCmd = &cobra.Command{
+	Use:   "completion [bash|zsh|fish|powershell]",
+	Short: "Generate completion script",
+	Long: `To load completions:
+Bash:
+$ source <(gotop completion bash)
+# To load completions for each session, execute once:
+Linux:
+	$ gotop completion bash > /etc/bash_completion.d/gotop 
+MacOS:
+	$ gotop completion bash > /usr/local/etc/bash_completion.d/gotop 
+Zsh:
+# If shell completion is not already enabled in your environment you will need
+# to enable it.  You can execute the following once:
+$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+# To load completions for each session, execute once:
+$ gotop completion zsh > "${fpath[1]}/_gotop"
+# You will need to start a new shell for this setup to take effect.
+Fish:
+$ gotop completion fish | source
+# To load completions for each session, execute once:
+$ gotop completion fish > ~/.config/fish/completions/gotop.fish
+`,
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletion(os.Stdout)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(completionCmd)
+}
