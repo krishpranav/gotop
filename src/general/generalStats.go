a274cebf775081b53d2ac3d22696d3a625445feb
krishpranav <krisna.pranav@gmail.com>
2021-07-02 08:07:36 +0530

    general stats added with cpu usage, network usage etc...

--- src/general/generalStats.go
+++ src/general/generalStats.go
@@ -8,3 +8,37 @@ import (
 )
 
 type serveFunc func(context.Context, chan utils.DataStats) error
+
+func GlobalStats(ctx context.Context, dataChannel chan utils.DataStats, refreshRate uint64) error {
+
+	serveFuncs := []serveFunc{
+		ServeCPURates,
+		ServeMemRates,
+		ServeDiskRates,
+		ServeNetRates,
+	}
+
+	return utils.TickUntilDone(ctx, int64(refreshRate), func() error {
+		var wg sync.WaitGroup
+
+		errCh := make(chan error, len(serveFuncs))
+
+		for _, sf := range serveFuncs {
+			wg.Add(1)
+			go func(sf serveFunc, dc chan utils.DataStats) {
+				defer wg.Done()
+				errCh <- sf(ctx, dc)
+			}(sf, dataChannel)
+		}
+
+		wg.Wait()
+		close(errCh)
+		for err := range errCh {
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
+}
